7fd98df2c5bfbdcc611385219829555e13105198
maritza mardhatillah <86137893+maritzamar@users.noreply.github.com>
2021-09-26 20:31:37 +0000

    JavaScript

---
+++ javascript.js
@@ -0,0 +1,32 @@
+//menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+    tombolMenu.click(function () {
+        menu.toggle();
+    });
+    menu.click(function () {
+        menu.toggle();
+    });
+}
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if (width < 990) {
+        klikMenu();
+    }
+})
+
+//check lebar
+$(window).resize(function () {
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display", "block");
+        //display:block
+    } else {
+        menu.css("display", "none");
+    }
+    klikMenu();
+});
+
