65a30077fe900ebbe36df894a22003eb5e38d851
maritza mardhatillah <86137893+maritzamar@users.noreply.github.com>
2021-10-08 17:17:21 +0000

    carousel

--- javascript.js
+++ javascript.js
@@ -30,3 +30,20 @@ $(window).resize(function () {
     klikMenu();
 });
 
+$(document).ready(function () {
+    $("#autoWidth").lightSlider({
+      adaptiveHeight: true,
+      auto: true,
+      item: 1,
+      slideMargin: 0,
+      loop: true
+    });
+  });
+  
+  const navbarHome = document.querySelector(".navbarHome");
+  
+  window.addEventListener("scroll", () => {
+    const post = window.scrollY > 100;
+  
+    navbarHome.classList.toggle("scroll", post);
+  });
